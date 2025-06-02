9e0b256dce1a614dd58ac544db8b0602d4a5e968
Daren Salazar <sdaren6@gmail.com>
2025-06-02 11:55:31 -0500

    feat: update movements view with filtering and search functionality for improved data management

--- src/views/movements/Index.vue
+++ src/views/movements/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Movimientos</h1>
   <DataTable
-    :value="dataTable"
+    :value="filteredMovements"
     paginator
     :rows="10"
     data-key="id"
@@ -11,22 +11,29 @@
   >
     <template #header>
       <div class="flex justify-between items-center gap-2">
+        <Calendar
+          v-model="dateRange"
+          selection-mode="range"
+          show-icon
+          placeholder="Selecciona un rango de fechas"
+          class="w-64"
+          :max-date="new Date()"
+        />
         <div class="flex gap-2">
-          <Calendar
-            v-model="dateRange"
-            selection-mode="range"
-            show-icon
-            placeholder="Selecciona un rango de fechas"
-            class="w-64"
-            :max-date="new Date()"
+          <Select
+            v-model="movementTypeFilter"
+            :options="listMovementTypes"
+            placeholder="Seleccionar Fondo"
+            style="width: 210px"
+            show-clear
           />
+          <IconField>
+            <InputIcon>
+              <i class="pi pi-search" />
+            </InputIcon>
+            <InputText v-model="search" placeholder="Buscar" />
+          </IconField>
         </div>
-        <IconField>
-          <InputIcon>
-            <i class="pi pi-search" />
-          </InputIcon>
-          <InputText placeholder="Buscar..." />
-        </IconField>
       </div>
     </template>
 
@@ -49,11 +56,12 @@
 
 <script setup>
 import { formatterMoney } from "@/composables/useUtils";
-import { ref, onMounted, watch } from "vue";
+import { computed, ref, onMounted, watch } from "vue";
 import { authAxios } from "@/composables/useAxios";
 import { useNotify } from "@/composables/useNotify";
 import { apiV1 } from "@/config/endpoints";
 import { DateTime } from "luxon";
+import { uniqBy } from "lodash-es";
 
 const toast = useNotify();
 
@@ -63,6 +71,28 @@ const startDate = endDate.plus({ months: -1 });
 const dateRange = ref([startDate.toJSDate(), endDate.toJSDate()]);
 const dataTable = ref([]);
 const loadingData = ref(false);
+const search = ref("");
+const movementTypeFilter = ref(null);
+const listMovementTypes = ref([]);
+
+const filteredMovements = computed(() => {
+  return dataTable.value.filter((mov) => {
+    const searchLower = search.value.toLowerCase();
+
+    const matchesSearch = search.value
+      ? mov.fundName?.toLowerCase().includes(searchLower) ||
+        mov.documentType?.toLowerCase().includes(searchLower) ||
+        mov.tradeName?.toLowerCase().includes(searchLower) ||
+        mov.expenseType?.toLowerCase().includes(searchLower) ||
+        mov.notes?.toLowerCase().includes(searchLower) ||
+        mov.amount?.toString().includes(searchLower)
+      : true;
+
+    const matchesMovementType = movementTypeFilter.value ? mov.movementType === movementTypeFilter.value : true;
+
+    return matchesSearch && matchesMovementType;
+  });
+});
 
 const getDataTable = async () => {
   try {
@@ -80,6 +110,7 @@ const getDataTable = async () => {
       });
     }
     dataTable.value = data;
+    listMovementTypes.value = uniqBy(data.map((e) => e.movementType));
   } catch {
     toast.notify({ severity: "error", summary: "Error", detail: "Error al obtener los tipos gastos" });
   } finally {
